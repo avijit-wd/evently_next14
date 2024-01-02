f5bcba76faf9f68124109a35ebd7855035c08e20
Avijit Biswas <avijit@devdynamics.ai>
2024-01-02 13:23:48 +0530

    middleware changed

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/", "/events/:id", "/api/webhook/stripe", "/api/uploadthing"],
-  ignoredRoutes: ["/events/:id", "/api/webhook/stripe", "/api/uploadthing"],
+  ignoredRoutes: [
+    "/events/:id",
+    "/api/webhook/stripe",
+    "/api/webhook/clerk",
+    "/api/uploadthing",
+  ],
 });
 
 export const config = {
